fcdb8d385ad76047c686cd537716af3f2631892e
dev.olvericc <eric.lopes@salvador.ba.gov.br>
2023-09-13 09:35:28 -0300

    updating with a example using constructor

---
+++ 12092023-AccessModifiersAndConstructors/example/src/application/Constructor.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.ConstructorClass;
+
+public class Constructor {
+
+    public static void main(String[] args) {
+
+        ConstructorClass cc1 = new ConstructorClass();
+
+        System.out.println(cc1);
+
+    }
+
+}
---
+++ 12092023-AccessModifiersAndConstructors/example/src/entities/ConstructorClass.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class ConstructorClass
+{
+    public ConstructorClass()
+    {
+        System.out.println("test");
+    }
+}
